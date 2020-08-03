29b41f6795d046375d100a408e33859926c6ecc3
Super Shimonster <supershimonster@gmail.com>
2020-08-03 00:31:42 -0700

    worked on multiple clients

--- server.dart
+++ server.dart
@@ -4,25 +4,57 @@ import 'dart:async';
 import 'dart:isolate';
 
 void main(List<String> args) {
-  final isolates = Isolates();
+// final isolates = Isolates();
+  final mainPort = ReceivePort();
+  List<ClientIsolate> clientIsolates = [];
+  Map<String, SendPort> clientPorts = {};
   print('server started');
   HttpServer.bind('localhost', 16969).then(
+    /// creates the server ^^^^^^^^^^^
     (server) async {
+      /// when created the [server] <---------
       print('server set up as listening: ${server.port}');
-      await isolates.spawnInputIsolate();
+      await Isolate.spawn(inputIsolate, {'allClients': clientIsolates});
+      //  {'allClients': clientIsolates, 'mainPort': mainPort.sendPort});
+      /// await isolates.spawnInputIsolate();
       server.listen(
         (request) {
+          /// This trigers when a [request] is sent <---------
           WebSocketTransformer.upgrade(request).then(
+            /// upgreades the request to a web socket ^^^^^^^^^^^^^^
             (ws) async {
+              /// when upgraded [ws] is the web socket <---------
+              /// sends initial data to client web socket when created <--------
               print('server recieved request');
               print(request.uri);
-              await isolates.spawnClientIsolate(
-                  request.uri.pathSegments[0], ws);
+              final uid = request.uri.pathSegments[0];
+              clientIsolates.add(ClientIsolate(uid));
+              clientIsolates
+                  .firstWhere((element) => element.uid == uid)
+                  .awaitInit
+                  .then((clientSendPort) {
+                clientPorts.putIfAbsent(uid, () => clientSendPort);
+                clientSendPort.send({'ws': ws});
+              });
+// await isolates.spawnClientIsolate(
+//     request.uri.pathSegments[0], ws);
               if (ws.readyState == WebSocket.open) {
+                /// if [ws] is open, do this stuff <---------
                 ws.listen(
                   (event) {
+                    /// listens to [event]s from the web socket <---------
                     print(
                         '${DateTime.now()}, ${Map<String, String>.from(json.decode(event))}');
+
+                    final wsMessage =
+                        Map<String, String>.from(json.decode(event))
+                            .values
+                            .toString()[0];
+
+                    clientIsolates.removeWhere((isl) => uid == isl.uid);
+                    clientIsolates.forEach((isl) {
+                      clientPorts[uid].send(wsMessage);
+                    });
                   },
                   onDone: () => print('done'),
                   onError: (error) =>
@@ -41,7 +73,107 @@ void main(List<String> args) {
   );
 }
 
-class Isolates {
+void inputIsolate(Map<String, dynamic> message) {
+  while (true) {
+    final input = stdin.readLineSync();
+    message['allClients'].forEach((value) {
+      value.send(input);
+    });
+  }
+}
+
+clientIsolate(SendPort mainSendPort) async {
+  final thisRecievePort = ReceivePort();
+  mainSendPort.send(thisRecievePort.sendPort);
+  final Map<String, dynamic> first = await thisRecievePort.first;
+  final thisWs = first['ws'];
+  print('isolate init');
+
+  // thisWs.add(json.encode({DateTime.now().toString(): 'recieveMessage'}));
+  thisRecievePort.listen((recieveMessage) {
+    thisWs.add(json.encode({DateTime.now().toString(): recieveMessage}));
+  });
+}
+
+class ClientIsolate {
+  ClientIsolate(this.uid) {
+    spawnClientIsolate().then((value) {
+      _internalPort.listen((message) {
+        isolateSendPort = message;
+      });
+    });
+  }
+
+  final _internalPort = ReceivePort();
+  SendPort isolateSendPort;
+  final String uid;
+
+  Future<SendPort> get awaitInit async {
+    while (isolateSendPort == null) {}
+    return isolateSendPort;
+  }
+
+  Future<void> spawnClientIsolate() async {
+    await Isolate.spawn(clientIsolate, _internalPort.sendPort);
+  }
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+class Isolates1 {
   final mainPort = ReceivePort();
   Map<String, SendPort> clientPorts = {};
 
@@ -61,14 +193,13 @@ class Isolates {
     await Isolate.spawn(inputIsolate, clientPorts);
   }
 
-  static clientIsolate(message) {
-    final WebSocket thisWs = message['ws'];
+  static clientIsolate(message) async {
+    final thisRecievePort = ReceivePort();
     final String thisId = message['clientId'];
     final SendPort mainSendPort = message['mainPort'];
     final Map<String, SendPort> allSendPorts = message['allSendPorts'];
-    final thisRecievePort = ReceivePort();
-
     mainSendPort.send({'port': thisRecievePort.sendPort, 'id': thisId});
+    final WebSocket thisWs = await thisRecievePort.first;
 
     thisRecievePort.listen((recieveMessage) {
       thisWs.add(json.encode({DateTime.now().toString(): recieveMessage}));
@@ -87,10 +218,10 @@ class Isolates {
 
   Future<void> spawnClientIsolate(String clientId, WebSocket ws) async {
     await Isolate.spawn(clientIsolate, {
-      'ws': ws,
       'clientId': clientId,
       'allSendPorts': clientPorts,
       'mainPort': mainPort.sendPort
     });
+    clientPorts[clientId].send(ws);
   }
 }
